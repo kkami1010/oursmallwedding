043362721a2920c59f46f73b65ca687852b9e337
kkami1010 <khyun0@gmail.com>
2026-07-23 17:10:16 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -22,7 +22,7 @@
     const day = days[d.getDay()];
     const hours = d.getHours();
     const minutes = d.getMinutes();
-    const period = hours < 12 ? '오전' : '오후';
+    const period = hours < 12 ? '오전' : hours === 12 ? '낮' : '오후';
     const h12 = hours % 12 || 12;
     const minuteStr = minutes > 0 ? ` ${minutes}분` : '';
     return `${year}년 ${month}월 ${date}일 ${day}요일 ${period} ${h12}시${minuteStr}`;
